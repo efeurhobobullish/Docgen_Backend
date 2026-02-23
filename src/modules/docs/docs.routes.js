9dbad7044caa08d3233c9a76b57932683a9de642
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2026-02-23 19:43:00 +0100

    Add routes for document version management

--- src/modules/docs/docs.routes.js
+++ src/modules/docs/docs.routes.js
@@ -1,9 +1,46 @@
 import { Router } from "express";
 import { protect } from "../../middlewares/auth.middleware.js";
-import { generateReadme } from "./docs.controller.js";
+import {
+  generateReadme,
+  getLatestDoc,
+  getAllVersions,
+  getSpecificVersion,
+  deleteVersion,
+} from "./docs.controller.js";
 
 const router = Router();
 
+/* ======================
+   GENERATE README
+====================== */
 router.post("/:repoId/generate-readme", protect, generateReadme);
+
+/* ======================
+   GET LATEST README
+====================== */
+router.get("/:repoId", protect, getLatestDoc);
+
+/* ======================
+   GET ALL VERSIONS
+====================== */
+router.get("/:repoId/versions", protect, getAllVersions);
+
+/* ======================
+   GET SPECIFIC VERSION
+====================== */
+router.get(
+  "/:repoId/version/:version",
+  protect,
+  getSpecificVersion
+);
+
+/* ======================
+   DELETE VERSION
+====================== */
+router.delete(
+  "/:repoId/version/:version",
+  protect,
+  deleteVersion
+);
 
 export default router;
