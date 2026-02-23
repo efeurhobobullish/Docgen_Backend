593400959ed8b84a0e41e2fd7d5fffdb8bf0349f
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2026-02-23 19:24:21 +0100

    Update repo.service.js

--- src/modules/repo/repo.service.js
+++ src/modules/repo/repo.service.js
@@ -1,4 +1,40 @@
 import RepositoryModel from "./repo.model.js";
+import axios from "axios";
+import UserModel from "../../models/user.model.js";
+import RepositoryModel from "./repo.model.js";
+
+export const getRepoTreeService = async (userId, repoId) => {
+  const repository = await RepositoryModel.findOne({
+    _id: repoId,
+    user: userId,
+  });
+
+  if (!repository) {
+    throw new Error("Repository not found");
+  }
+
+  const user = await UserModel.findById(userId).select("+githubAccessToken");
+
+  if (!user.githubAccessToken) {
+    throw new Error("GitHub not connected");
+  }
+
+  const response = await axios.get(
+    `https://api.github.com/repos/${repository.fullName}/git/trees/${repository.defaultBranch}?recursive=1`,
+    {
+      headers: {
+        Authorization: `Bearer ${user.githubAccessToken}`,
+      },
+    }
+  );
+
+  const files = response.data.tree
+    .filter((item) => item.type === "blob")
+    .map((item) => item.path);
+
+  return files;
+};
+
 
 export const connectRepoService = async (userId, repoData) => {
   const exists = await RepositoryModel.findOne({
