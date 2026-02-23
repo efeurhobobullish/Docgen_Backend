6ce05e52e228fc6fcdd5ecb4d3a4333ddd638b6e
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2026-02-23 19:05:57 +0100

    Update auth.middleware.js

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,36 @@
+import { verifyToken } from "../utils/jwt.js";
+import UserModel from "../models/user.model.js";
+
+export const protect = async (req, res, next) => {
+  try {
+    let token;
+
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+
+    if (!token) {
+      return res.status(401).json({ message: "Not authorized" });
+    }
+
+    const decoded = verifyToken(token);
+
+    const user = await UserModel.findById(decoded.id);
+
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    if (user.isBlocked) {
+      return res.status(403).json({ message: "Account blocked" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
