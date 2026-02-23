08809ee323c0492c2e21202b418b94a8fc4b08c2
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2026-02-23 19:12:49 +0100

    Add route to get GitHub repositories

--- src/modules/github/github.routes.js
+++ src/modules/github/github.routes.js
@@ -2,11 +2,14 @@ import { Router } from "express";
 import {
   redirectToGithub,
   githubCallback,
+  getRepos,
 } from "./github.controller.js";
+import { protect } from "../../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.get("/auth", redirectToGithub);
 router.get("/callback", githubCallback);
+router.get("/repos", protect, getRepos);
 
 export default router;
