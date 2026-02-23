35c4b4804299e53c1aca371a37ba73b1b913032c
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2026-02-23 19:36:43 +0100

    Update docgen.worker.js

--- src/queue/docgen.worker.js
+++ src/queue/docgen.worker.js
@@ -1,1 +1,13 @@
 
+import { Worker } from "bullmq";
+import { redisConnection } from "./connection.js";
+import { generateReadmeService } from "../modules/docs/docs.service.js";
+
+export const docgenWorker = new Worker(
+  "docgen",
+  async (job) => {
+    const { userId, repoId } = job.data;
+    await generateReadmeService(userId, repoId);
+  },
+  { connection: redisConnection }
+);
