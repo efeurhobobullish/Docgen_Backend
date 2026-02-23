c034075c253cd39e24654370156219fbd4017b6e
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2026-02-23 18:42:53 +0100

    Update app.js

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+app.get("/", (req, res) => {
+  res.json({ message: "DocGen API running 🚀" });
+});
+
+export default app;
