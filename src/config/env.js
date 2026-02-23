ad6199718bd1807ca1e1333a1f94524020848dc3
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2026-02-23 18:42:13 +0100

    Update env.js

---
+++ src/config/env.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+import process from "process";
+dotenv.config();
+
+export const env = {
+  port: process.env.PORT || 5000,
+  mongoUri: process.env.MONGO_URI,
+  jwtSecret: process.env.JWT_SECRET,
+  jwtExpiresIn: process.env.JWT_EXPIRES_IN,
+
+  githubClientId: process.env.GITHUB_CLIENT_ID,
+  githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
+  githubCallbackUrl: process.env.GITHUB_CALLBACK_URL,
+};
